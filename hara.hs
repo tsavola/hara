e93d9d4b9266c13a062e6e00945730d591dd5e06
Timo Savola <timo.savola@iki.fi>
2015-01-03 00:48:18 +0200

    square output with 6x10-pixel font

--- hara.hs
+++ hara.hs
@@ -2,8 +2,8 @@ type Vector = (Double, Double, Double)
 
 newtype Color = Color { char :: Char }
 
-width = 80
-height = 40
+width = 100
+height = 60
 
 sphereOrigin :: Vector
 sphereOrigin = (0, 0, -2)
